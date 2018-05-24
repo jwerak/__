052823501c756efc7718ffdad4cb84ea7c1cbd40
Jiri Bramburek <jiri.bramburek@sap.com>
2018-05-24 22:00:26 +0200

    refactoring and simplification

--- cocktail.py
+++ cocktail.py
@@ -3,79 +3,60 @@
 from gpiozero import OutputDevice
 import time
 
-led6 = OutputDevice(6)
-led13 = OutputDevice(13)
-led19 = OutputDevice(19)
-led26 = OutputDevice(26)
 
-SMALL_SHOT = 1.5
-BIG_SHOT = 2.8
+class Ingredient(OutputDevice):
 
-receipts = {
+    def __init__(self, name, *args, **kwargs):
+        self.name = name
+        super(Ingredient, self).__init__(*args, **kwargs)
+
+    def infuse(self, seconds):
+        print('Infusing {} ({}) for {} seconds', self.name, self.pin, seconds)
+        self.on()
+        time.sleep(seconds)
+        self.off()
+        print('Infusion of {} completed', self.name)
+
+
+_ingredients = [
+    Ingredient('rum', pin=6, active_high=False),
+    Ingredient('cola', pin=13, active_high=False),
+    Ingredient('gin', pin=19, active_high=False),
+    Ingredient('tonic', pin=26, active_high=False)]
+
+
+_receipts = {
     'cubalibre': [('rum', 3), ('cola', 25)],
-    'gintonic': [('gin', 3), ('tonic', 25)]
-}
+    'gintonic': [('gin', 3), ('tonic', 25)]}
 
 
-def turn_off():
-    led6.on()
-    led13.on()
-    led19.on()
-    led26.on()
-    time.sleep(5)
+def _all_off():
+    for i in _ingredients:
+        i.off()
 
 
-def turn_on():
-    led6.off()
-    led13.off()
-    led19.off()
-    led26.off()
-    time.sleep(5)
-
-
-def pump_ingredients(name, duration):
-    if name == 'rum':
-        print "Rum for {} seconds".format(duration)
-        led6.off()
-        time.sleep(duration)
-        led6.on()
-    if name == 'cola':
-        print "Cola for {} seconds".format(duration)
-        led13.off()
-        time.sleep(duration)
-        led13.on()
-    if name == 'gin':
-        print "Gin for {} seconds".format(duration)
-        led19.off()
-        time.sleep(duration)
-        led19.on()
-    if name == 'tonic':
-        print "Tonic for {} seconds".format(duration)
-        led26.off()
-        time.sleep(duration)
-        led26.on()
+def _all_on():
+    for i in _ingredients:
+        i.on()
 
 
 def drinks():
-    return receipts
+    return _receipts
 
 
 def ingredients():
-    ing = []
-    for r in receipts:
-        for i in receipts[r]:
-            ing.append(i[0])
-    return ing
+    return [i.name for i in _ingredients]
 
 
 def serve_drink(name):
-    print("Serving drink: {0}".format(name))
-    for ingredients in receipts[name]:
-        pump_ingredients(ingredients[0], ingredients[1])
+    print('Serving drink: {0}'.format(name))
+    for ingredient, duration in _receipts[name]:
+        for i in [i for i in _ingredients if i.name == name]:
+            i.infuse(duration)
 
 
-print("Ready to prepare these drinks:")
+print('Ready to prepare these drinks:')
 print drinks()
-print("from these ingredients")
+print('from these ingredients:')
 print ingredients()
-turn_off()
+_all_off()
