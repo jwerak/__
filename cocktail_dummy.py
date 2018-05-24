be64ba3e14d7c30288dd000216abdce4a5e7fa19
Jakub Veverka <jakub.veverka@concur.com>
2018-05-24 15:52:52 +0200

    Add slackbot and dummy cocktail function

---
+++ cocktail_dummy.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python2
+
+
+def ServeDrink(name):
+    print("Serving drink: {0}".format(name))
+
+
+if __name__ == "__main__":
+    ServeDrink("Virgin cuba libre")
---
+++ slackbot.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+
+from cocktail_dummy import ServeDrink
+
+ServeDrink("Caipirinha")
